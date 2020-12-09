800e961b78192f0587c8e0ad0714a513eb08f3ed
raazdevanuj <raazdevanuj@gmail.com>
2020-12-09 10:37:43 +0530

    after complete the design part

--- myscripts.js
+++ myscripts.js
@@ -5,3 +5,14 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+function Validate()
+{
+var e = document.getElementById("dep");
+var strUser = e.options[e.selectedIndex].value;
+
+var strUser1 = e.options[e.selectedIndex].text;
+if(strUser==-1)
+{
+alert("Please select a Department");
+}
+}
